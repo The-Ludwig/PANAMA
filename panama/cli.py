3238eb7e33b26471ab01ec784a6a35a3391dd6a3
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2023-02-08 15:38:39 +0100

    restructured files

---
+++ panama/cli.py
@@ -0,0 +1,111 @@
+import click
+from .parallel_run import run_corsika_parallel
+
+DEFAULT_TMP_DIR = environ.get("TMP_DIR", "/tmp/PANAMA")
+CORSIKA_PATH = environ.get(
+    "CORSIKA_PATH",
+    f"{environ.get('HOME')}/corsika7-master/run/corsika77420Linux_SIBYLL_urqmd",
+)
+DEFAULT_N_EVENTS = 100
+
+
+@click.command(context_settings={"show_default": True})
+@click.argument(
+    "template", type=click.Path(exists=True, dir_okay=False)
+)  # , help="Path to the template to run")
+@click.option(
+    "--events",
+    "-n",
+    type=int,
+    help="Number of shower-events to generate per primary particle.",
+    default=DEFAULT_N_EVENTS,
+)
+@click.option(
+    "--primary",
+    "-p",
+    type=int | str,
+    help="PDGid of primary to inject. Default is proton. "
+    "Can be a python dict with diffrerent primaries as keys and values the number of events to generate for that. "
+    "In this case, --events is ignored. "
+    "Example with proton and iron: {2212: 100_000, 1000260560: 1000}",
+    default=2212,
+)
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(),
+    help="Path to store the CORSIKA7 DAT files",
+    default="./corsika_output/",
+)
+@click.option("--jobs", "-j", default=4, help="Number of jobs to use", type=int)
+@click.option(
+    "--corsika",
+    "-c",
+    default=CORSIKA_PATH,
+    help="Path to the CORSIKA7 executable. Can also be set using the `CORSIKA_PATH` environment variable.",
+    type=click.Path(exists=True, dir_okay=False, executable=True),
+)
+@click.option(
+    "--seed",
+    "-s",
+    default=None,
+    help="Seed to use. If none, will use system time or other entropic source",
+    type=int,
+)
+@click.option(
+    "--tmp",
+    "-t",
+    default=DEFAULT_TMP_DIR,
+    type=click.Path(file_okay=False),
+    help="Path to the default temp folder to copy corsika to. Can also be set using the `TMP_DIR` environment variable.",
+)
+@click.option("--debug", "-d", default=False, is_flag=True, help="Enable debug output")
+def cli(
+    template: Path,
+    events: int,
+    output: Path,
+    jobs: int,
+    corsika: Path,
+    seed: int,
+    tmp: Path,
+    debug,
+):
+    """
+    Command line interface to run CORSIKA7 in parallel.
+
+    The `TEMPLATE` argument must point to a valid CORSIKA7 stiring card, where
+    `{run_idx}`, `{first_event_idx}` `{n_show}` `{seed_1}` `{seed_2}` and `{dir}`
+    will be replaced accordingly.
+
+    For examples see the PANAMA repository:
+
+    https://github.com/The-Ludwig/PANAMA#readme
+    """
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if tmp == DEFAULT_TMP_DIR:
+        n = 0
+        p = Path(tmp)
+        while p.exists() and next(p.iterdir(), True) != True:
+            n += 1
+            p = Path(DEFAULT_TMP_DIR + f"_{n}")
+    else:
+        p = Path(tmp)
+
+    if isinstance(primary, int):
+        primary = {primary: events}
+    elif isinstance(primary, str):
+        primary = eval(primary)
+        if events != DEFAULT_N_EVENTS:
+            logging.warn(
+                f"Looks like --events was given and --primary was provided a dict. --events is ignored."
+            )
+    else:
+        raise TypeError("Primary must either be int or string")
+
+    run_corsika_parallel(primary, jobs, template, Path(output), corsika, p, seed)
+
+
+if __name__ == "__main__":
+    cli()
--- panama/parallel_run.py
+++ panama/parallel_run.py
@@ -4,12 +4,12 @@ from os import environ
 from pathlib import Path
 from random import randrange
 from random import seed as set_seed
-import click
 import logging
 from tqdm import tqdm
 from time import sleep
 import shutil
 from .nbstreamreader import NonBlockingStreamReader as NBSR
+from particle import Corsika7ID
 
 CORSIKA_FILE_ERROR = "STOP FILOPN: FATAL PROBLEM OPENING FILE"
 CORSIKA_EVENT_FINISHED = b"PRIMARY PARAMETERS AT FIRST INTERACTION POINT AT HEIGHT"
@@ -23,6 +23,7 @@ def start_corsika_job(
     abs_corsika_path: Path,
     output: Path,
     corsika_tmp_dir: Path,
+    primary_corsikaid: int,
     first_event_idx: int = 1,
 ) -> Popen:
     # create dir if not existent
@@ -31,6 +32,8 @@ def start_corsika_job(
     corsika_copy_dir = corsika_tmp_dir / f".corsika_copy_run_{run_idx}"
     corsika_copy_dir.mkdir(parents=True, exist_ok=True)
 
+    # copy all files in the corsika run dir, except for the DAT files
+    # which are often created, if you test corsika in the run dir
     for p in abs_corsika_path.parent.glob("[!DAT]*"):
         if not p.is_file():
             continue
@@ -53,6 +56,7 @@ def start_corsika_job(
                 dir=str(output.absolute()) + "/",
                 seed_1=f"{randrange(1, 900_000_000)}",
                 seed_2=f"{randrange(1, 900_000_000)}",
+                primary=f"{primary_corsikaid}",
             ).encode("ASCII"),
             timeout=1,
         )
@@ -63,13 +67,13 @@ def start_corsika_job(
     return job
 
 
-def cleanup(n_jobs: int, corsika_tmp_dir: Path):
-    for i in range(n_jobs):
+def cleanup(n_runs: int, corsika_tmp_dir: Path):
+    for i in range(n_runs):
         shutil.rmtree(corsika_tmp_dir / f".corsika_copy_run_{i}")
 
 
 def run_corsika_parallel(
-    n_events,
+    primary: dict,
     n_jobs,
     template_path,
     output: Path,
@@ -83,27 +87,40 @@ def run_corsika_parallel(
     with open(template_path) as f:
         input_template: str = f.read()
 
-    events_per_job = n_events // n_jobs
-    last_events_per_job = events_per_job + (n_events - events_per_job * n_jobs)
-
     abs_path = Path(corsika_path).absolute()
     jobs: [Popen] = []
-    for i in range(n_jobs - 1):
+
+    for idx, (pdgid, n_events) in enumerate(primary.items()):
+        corsikaid = int(Corsika7id.from_pdgid(pdgid))
+
+        events_per_job = n_events // n_jobs
+        last_events_per_job = events_per_job + (n_events - events_per_job * n_jobs)
+
+        for i in range(n_jobs - 1):
+            jobs.append(
+                start_corsika_job(
+                    idx + i,
+                    events_per_job,
+                    input_template,
+                    abs_path,
+                    output,
+                    corsika_tmp_dir,
+                    corsikaid,
+                )
+            )
         jobs.append(
             start_corsika_job(
-                i, events_per_job, input_template, abs_path, output, corsika_tmp_dir
+                idx + (n_jobs - 1),
+                last_events_per_job,
+                input_template,
+                abs_path,
+                output,
+                corsika_tmp_dir,
+                corsikaid,
             )
         )
-    jobs.append(
-        start_corsika_job(
-            n_jobs - 1,
-            last_events_per_job,
-            input_template,
-            abs_path,
-            output,
-            corsika_tmp_dir,
-        )
-    )
+
+    n_events = sum(primary.values())
 
     # show progressbar until close to end
     finished_events = 0
@@ -142,92 +159,4 @@ def run_corsika_parallel(
             )
 
     print("All jobs terminated, cleanup now")
-    cleanup(n_jobs, corsika_tmp_dir)
-
-
-DEFAULT_TMP_DIR = environ.get("TMP_DIR", "/tmp/PANAMA")
-CORSIKA_PATH = environ.get(
-    "CORSIKA_PATH",
-    f"{environ.get('HOME')}/corsika7-master/run/corsika77420Linux_SIBYLL_urqmd",
-)
-
-
-@click.command(context_settings={"show_default": True})
-@click.argument(
-    "template", type=click.Path(exists=True, dir_okay=False)
-)  # , help="Path to the template to run")
-@click.option(
-    "--events",
-    "-n",
-    type=int,
-    help="Number of shower-events to generate",
-    default=100,
-)
-@click.option(
-    "--output",
-    "-o",
-    type=click.Path(),
-    help="Path to store the CORSIKA7 DAT files",
-    default="./corsika_output/",
-)
-@click.option("--jobs", "-j", default=4, help="Number of jobs to use", type=int)
-@click.option(
-    "--corsika",
-    "-c",
-    default=CORSIKA_PATH,
-    help="Path to the CORSIKA7 executable. Can also be set using the `CORSIKA_PATH` environment variable.",
-    type=click.Path(exists=True, dir_okay=False, executable=True),
-)
-@click.option(
-    "--seed",
-    "-s",
-    default=None,
-    help="Seed to use. If none, will use system time or other entropic source",
-    type=int,
-)
-@click.option(
-    "--tmp",
-    "-t",
-    default=DEFAULT_TMP_DIR,
-    type=click.Path(file_okay=False),
-    help="Path to the default temp folder to copy corsika to. Can also be set using the `TMP_DIR` environment variable.",
-)
-@click.option("--debug", "-d", default=False, is_flag=True, help="Enable debug output")
-def cli(
-    template: Path,
-    events: int,
-    output: Path,
-    jobs: int,
-    corsika: Path,
-    seed: int,
-    tmp: Path,
-    debug,
-):
-    """
-    Command line interface to run CORSIKA7 in parallel.
-
-    The `TEMPLATE` argument must point to a valid CORSIKA7 stiring card, where
-    `{run_idx}`, `{first_event_idx}` `{n_show}` `{seed_1}` `{seed_2}` and `{dir}`
-    will be replaced accordingly.
-
-    For examples see the PANAMA repository:
-
-    https://github.com/The-Ludwig/PANAMA#readme
-    """
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
-
-    if tmp == DEFAULT_TMP_DIR:
-        n = 0
-        p = Path(tmp)
-        while p.exists() and next(p.iterdir(), True) != True:
-            n += 1
-            p = Path(DEFAULT_TMP_DIR + f"_{n}")
-    else:
-        p = Path(tmp)
-
-    run_corsika_parallel(events, jobs, template, Path(output), corsika, p, seed)
-
-
-if __name__ == "__main__":
-    cli()
+    cleanup(n_jobs * len(primary), corsika_tmp_dir)
