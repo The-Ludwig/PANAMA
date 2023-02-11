c8b776214d007d991c0b07fef2f5b7f3d575c3b6
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2023-02-11 13:39:05 +0100

    add corsika test

---
+++ admin/download_corsika.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CORSIKA=corsika-77420
+DOWNLOAD_LINK=https://web.iap.kit.edu/corsika/download/corsika-v770/$CORSIKA.tar.gz
+
+if [ ! -n "$CORSIKA_PW" ]
+then
+	>&2 echo "Please set env variable CORSIKA_PW to the corsika download pw"
+	exit 1
+fi
+
+if [ ! -d "$CORSIKA" ]
+then
+	if [ ! -f "${CORSIKA}.tar.gz" ]
+	then
+		wget --http-user corsika --http-password $CORSIKA_PW $DOWNLOAD_LINK
+	fi
+	tar -xf $CORSIKA.tar.gz
+fi
+
+echo "######### CONFIGURING CORSIKA #########"
+cp admin/config.h $CORSIKA/include
+cd $CORSIKA
+./coconut --batch
+
+echo "#########  INSTALLING CORSIKA  #########"
+./coconut --install
