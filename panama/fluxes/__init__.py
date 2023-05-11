3ed388a3ad562efa7ac5f62f866fff38934a8b7f
Ludwig Neste <31670556+The-Ludwig@users.noreply.github.com>
2023-05-11 21:45:25 +0200

    add GST model (#52)

--- panama/fluxes/__init__.py
+++ panama/fluxes/__init__.py
@@ -1,3 +1,19 @@
-from .cosmic_ray_fluxes import TIG, CosmicRayFlux, GlobalSplineFit, H3a, H4a, TIGCutoff
+from .cosmic_ray_fluxes import (
+    TIG,
+    CosmicRayFlux,
+    GlobalFitGST,
+    GlobalSplineFit,
+    H3a,
+    H4a,
+    TIGCutoff,
+)
 
-__all__ = ("H3a", "H4a", "TIG", "TIGCutoff", "GlobalSplineFit", "CosmicRayFlux")
+__all__ = (
+    "H3a",
+    "H4a",
+    "TIG",
+    "TIGCutoff",
+    "GlobalSplineFit",
+    "CosmicRayFlux",
+    "GlobalFitGST",
+)
