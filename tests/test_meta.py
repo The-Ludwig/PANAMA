ca0701c6cfd0440bcba29d157982542ab5a9fc9e
Ludwig Neste <31670556+The-Ludwig@users.noreply.github.com>
2023-11-08 20:45:15 -0500

    make pytables an optional dependency (#89)
    * update pre-commit hooks
    
    * make it executable via 'python -m panama'
    
    * make hdf an optional dependency
    
    * add error message if trying to save with hdf without pytables being installed
    
    * update README
    
    * add test for module not found
    
    * add no cover for unreachabel line with python 3.8

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,11 +1,40 @@
 from panama import __logo__, __version__
 from importlib.metadata import version
+import importlib
+import pytest
+from click.testing import CliRunner
+from panama.cli import cli
+from pathlib import Path
+
+SINGLE_TEST_FILE = Path(__file__).parent / "files" / "DAT000000"
 
 def test_logo():
     logo = __logo__
     assert len(logo) > 500
     assert "v" in logo
 
-
 def test_version():
     assert __version__ == version("corsika-panama").replace("+editable", "")
+
+
+@pytest.mark.filterwarnings("ignore::pandas.errors.PerformanceWarning")
+def test_cli_no_tables(pytestconfig, tmp_path, caplog, monkeypatch, test_file_path=SINGLE_TEST_FILE):
+    monkeypatch.setattr(importlib.util, "find_spec", lambda s: None)
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            "hdf5",
+            "--debug",
+            f"{test_file_path}",
+            f"{tmp_path}/output.hdf5",
+        ],
+        catch_exceptions=True
+    )
+
+    assert type(result.exception) == ImportError
+    assert "corsika-panama[hdf]" in result.exception.msg
+    assert "corsika-panama[hdf]" in caplog.text
+    assert result.exit_code == 1
